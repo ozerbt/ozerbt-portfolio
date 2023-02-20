6a39a0d611c953b12dd61c1517f1af1684627aab
ozerbt <54027925+ozerbt@users.noreply.github.com>
2023-02-20 14:18:10 -0500

    adding skills section

--- ozerbt-portfolio/src/pages/landingPage/HeroSection.tsx
+++ ozerbt-portfolio/src/pages/landingPage/HeroSection.tsx
@@ -1,86 +1,79 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import DeveloperName from "./DeveloperName";
+import Projects from "./Projects";
+import { projects } from "../utils/variables";
+
 
 const HeroSection: NextPage = () => {
-    return (
+  return (
     <>
-        <div className="wrapper">
-      <div id="navbar-container" className="w-[50%]">
-        <div id="navbar-options" className="flex w-[40%] justify-between">
-          <h1 className="m-3 font-semibold mt-5 ml-10">GITHUB</h1>
-          <h1 className="m-3 font-semibold mt-5">RESUME</h1>
-          <h1 className="m-3 font-semibold mt-5">CONTACT</h1>
+
+      <div id="navbar-container" className=" w-full md:mt-3 md:w-[50%]">
+        <div id="navbar-options" className="flex w-[90%] md:w-[40%] justify-between">
+          <h1 className="m-3 hover:underline transition-all hover:cursor-pointer text-[12px] md:text-lg font-semibold mt-5 ml-10">GITHUB</h1>
+          <h1 className="m-3 text-[12px] md:text-lg font-semibold mt-5">RESUME</h1>
+          <h1 className="m-3 text-[12px] md:text-lg font-semibold mt-5">CONTACT</h1>
         </div>
       </div>
 
-      <div id="hero-container" className="w-full h-full grid-rows-2">
+      <div id="hero-container" className="w-full h-full grid-rows-2 grid-cols-2">
         {/*OUTER CONTAINER*/}
-        <div className="flex h-[95vh] ">
-          <div className="row-span-1 flex justify-center items-center w-[50%]">
-            <div className="w-[80%] h-[90%] relative ">
-              <div
-                id="developer-name"
-                className="flex  flex-col w-full h-[70%] justify-center"
-              >
-                <h1
-                  className="text-[48px] mb-4 font-bold leading-[72px] tracking-[0.04em]"
-                >
-                  Behcet Ozer
-                </h1>
-                <h1
-                  className="text-[48px] font-bold leading-[72px] tracking-[0.04em]"
-                >
-                  Software Developer
-                </h1>
-              </div>
-              <div>
-                <div id="icon-container" className="relative ">
-                  <div className="absolute bottom-[500px] left-[80px]">
-                    <Image width={50} height={50} src="/icons/AngularIcon.svg" alt="angularIcon"/>
-                    </div>
-
-                    <div className="absolute bottom-[600px] left-[300px]">
-                    <Image width={50} height={50} src="/icons/AwsIcon.svg" alt="AWSIcon"/>
-                    </div>
-
-                    <div className="absolute bottom-[150px] left-[300px]">
-                    <Image width={100} height={100} src="/icons/MongoDBIcon.svg" alt="MongoIcon"/>
-                    </div>
-
-                    <div className="absolute left-[200px] bottom-[20px]">
-                    <Image width={50} height={50} src="/icons/NextIcon.svg" alt="NextIcon"/>
-                    </div>
-                    <div className="absolute bottom-[140px]">
-                    <Image width={50} height={50} src="/icons/NodeIcon.svg" alt="NodeIcon"/>
-                    </div>
-                    <div className="absolute left-[500px]">
-                    <Image width={50} height={50} src="/icons/ReactIcon.svg" alt="ReactIcon"/>
-                    </div>
-                    <div className="absolute bottom-[550px] left-[550px]">
-                    <Image width={50} height={50} src="/icons/SqlIcon.svg" alt="SqlIcon"/>
-                    </div>
-                    <div className="absolute bottom-[200px] left-[600px]">
-                    <Image width={50} height={50} src="/icons/VueIcon.svg" alt="VueIcon"/>
-                    </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row-span-1 w-[50%] relative bg-green-500">
-            <div id="projects-container" className="w-[70%] absolute h-[110%] rotate-[18deg] bg-blue-400">
-              <div id="projects" className="flex"> 
-                <div className="bg-yellow-500">
-                  hi
-                </div>
-                
-              </div>
-            </div>
-          </div>
+        <div className="flex md:flex-row flex-col md:h-[95vh]">
+          <div className="absolute opacity-[0.17] blur-3xl top-[200px] md:top-[200px] w-full h-[50%] bg-[#83FFD2]" />
+          <DeveloperName />
+          <Projects />
         </div>
       </div>
-    </div>
-  </>
-    )
+      <div id="skills-container" className="w-full grid-cols-2 mt-[2rem] md:mt-[10rem] mb-[3rem] h-full">
+        <div className="col-span-1 ">
+
+          <div className=" border-[1px_solid_rgba(0,0,0,0.06)] flex flex-col items-center shadow-[2px_4px_40px_rgba(131,255,210,0.25)] w-[500px] h-[300px]">
+
+            <div className="w-[90%] mt-3 flex justify-center items-center bg-[#83FFD2] h-[54px] rounded-[10px]">
+              <h1 className="text-[36px] font-bold leading-[54px] tracking-[0.04em]">Frontend</h1>
+            </div>
+            <div className="w-[90%] mt-3 bg-[url(../../public/images/codeBackdrop.svg)] h-[55%]">
+
+            </div>
+
+          </div>
+
+          <div className=" border-[1px_solid_rgba(0,0,0,0.06)] flex flex-col items-center shadow-[2px_4px_40px_rgba(131,255,210,0.25)] w-[500px] h-[300px]">
+
+            <div className="w-[90%] mt-3 flex justify-center items-center bg-[#83FFD2] h-[54px] rounded-[10px]">
+              <h1 className="text-[36px] font-bold leading-[54px] tracking-[0.04em]">Backend</h1>
+            </div>
+            <div className="w-[90%] mt-3 bg-[url(../../public/images/codeBackdrop.svg)] h-[55%]">
+
+            </div>
+
+          </div>
+
+          <div className=" border-[1px_solid_rgba(0,0,0,0.06)] flex flex-col items-center shadow-[2px_4px_40px_rgba(131,255,210,0.25)] w-[500px] h-[300px]">
+
+            <div className="w-[90%] mt-3 flex justify-center items-center bg-[#83FFD2] h-[54px] rounded-[10px]">
+              <h1 className="text-[36px] font-bold leading-[54px] tracking-[0.04em]">Tools</h1>
+            </div>
+            <div className="w-[90%] mt-3 bg-[url(../../public/images/codeBackdrop.svg)] h-[55%]">
+
+            </div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
+        <div className="relative w-full h-full">
+          <div className="absolute">
+            <Image width={100} height={100} className=" w-[90%] h-full md:w-[100%] md:h-[100%]" src="/images/codeWindow.svg" alt="codeBackground" />
+          </div>
+        </div>
+
+      </div>
+
+    </>
+  )
 }
 
 export default HeroSection;
