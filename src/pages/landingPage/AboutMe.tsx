8d148c54018c16ffea8ef735ae88f92082d364b8
ozerbt <54027925+ozerbt@users.noreply.github.com>
2023-02-28 10:48:53 -0500

    updated aboutme

--- src/pages/landingPage/AboutMe.tsx
+++ src/pages/landingPage/AboutMe.tsx
@@ -4,16 +4,24 @@ import { IoNewspaperOutline } from 'react-icons/io5'
 const AboutMe: React.FC = () => {
     return (
         <div id="aboutme" className="h-full mt-[20rem] md:mt-[10rem] mb-[10rem] w-full">
-            <div className="bg-[#83FFD2] h-[400px] md:h-[300px] grid grid-rows-[100px_repeat(1,minmax(0,1fr))_50px] md:grid-cols-[300px_repeat(1,minmax(0,1fr))_250px]">
+            <div className="bg-[#83FFD2] h-[900px] md:h-[700px] lg:h-[600px] 2xl:h-[500px] grid grid-rows-[100px_repeat(1,minmax(0,1fr))_50px] md:grid-cols-[300px_repeat(1,minmax(0,1fr))_250px]">
                 <div className="relative bottom-[200px] md:bottom-[60px] md:right-[30px]">
                     <div className="flex justify-start">
                         <Image width={400} height={200} className="w-full object-cover object-middle md:object-none h-[300px] md:w-[400px] md:h-[400px] rounded-[40px]" src="/images/me.jpg" alt="me" />
                     </div>
                 </div>
-                <div className="flex flex-col mb-[4rem] md:mb-0 md:mt-[4rem] md:ml-[6rem] w-[90%]  h-[50%] self-center">
-
-                    <p className="md:text-[24px] ml-7 md:ml-0 md:mt-10 text-center md:text-start font-semibold">Passionate software developer that loves to code and contribute to disruptive technologies. Lover of coffee, mediocre cyclist, hiking enthusiast. </p>
-                    <a target="_blank" href="https://drive.google.com/file/d/1Pk4My_kBeI8L-xl-0c_U5MKHH3ma0mb7/view?usp=share_link" className="button hover:cursor-pointer md:w-[20%] mt-[2.5rem] md:mt-6 ml-10 md:ml-0 self-center text-center hover:scale-95 transition-all shadow-md bg-[#ffffff] pt-3 pb-3 pl-6 pr-6 md:pl-12 md:pr-12 rounded-[7px]" rel="noreferrer"><button ><span className="inline-block mr-2 pt-1"><IoNewspaperOutline /></span>Resume</button></a>
+                <div className="flex flex-col mb-[10rem] md:mb-0 md:mt-[4rem] md:ml-[6rem] md:w-full w-[90%]  h-[50%] self-center">
+                    <div className="inline-block break-words">
+                        <p className="md:text-[16px] lg:text-[24px] ml-7 md:ml-0 md:mt-10 text-center md:text-start font-semibold">
+                            Hi there! My name is Behcet Ozer and I'm from Ankara, Turkey. I have a strong passion for development and enjoy exploring new ways to improve my skills in this area.
+                            When I'm not working on development projects, you can usually find me enjoying a good cup of coffee, exploring the great outdoors on a hike, or cycling through the city.
+                            <br />
+                            <br />
+                            I have always been fascinated by the power of technology to solve problems and make the world a better place, which is why I decided to pursue a career in development.
+                            Whether I'm working on a new website or developing an innovative app, I find great satisfaction in using my skills to create something useful and impactful.
+                        </p>
+                    </div>
+                    <a target="_blank" href="https://drive.google.com/file/d/1Pk4My_kBeI8L-xl-0c_U5MKHH3ma0mb7/view?usp=share_link" className="button hover:cursor-pointer md:w-[50%] lg:w-[20%] mt-[2.5rem] md:mt-6 ml-10 md:ml-0 self-center text-center hover:scale-95 transition-all shadow-md bg-[#ffffff] pt-3 pb-3 pl-6 pr-6 md:pl-12 md:pr-12 rounded-[7px]" rel="noreferrer"><button ><span className="inline-block mr-2 pt-1"><IoNewspaperOutline /></span>Resume</button></a>
                 </div>
                 <div className="relative flex justify-center md:flex-none">
                     <div className="md:bg-[white]  absolute mr-5 md:mr-0 justify-center md:top-[-20px] md:right-[40px] p-2 whitespace-nowrap rounded-[10px] flex md:w-[150%]">
