a591e9504e1699eecc1deb5bd34cbad9a56c583b
ozerbt <54027925+ozerbt@users.noreply.github.com>
2023-02-28 10:50:54 -0500

    typo fix

--- src/pages/landingPage/AboutMe.tsx
+++ src/pages/landingPage/AboutMe.tsx
@@ -13,12 +13,12 @@ const AboutMe: React.FC = () => {
                 <div className="flex flex-col mb-[10rem] md:mb-0 md:mt-[4rem] md:ml-[6rem] md:w-full w-[90%]  h-[50%] self-center">
                     <div className="inline-block break-words">
                         <p className="md:text-[16px] lg:text-[24px] ml-7 md:ml-0 md:mt-10 text-center md:text-start font-semibold">
-                            Hi there! My name is Behcet Ozer and I'm from Ankara, Turkey. I have a strong passion for development and enjoy exploring new ways to improve my skills in this area.
-                            When I'm not working on development projects, you can usually find me enjoying a good cup of coffee, exploring the great outdoors on a hike, or cycling through the city.
+                            Hi there! My name is Behcet Ozer and im from Ankara, Turkey. I have a strong passion for development and enjoy exploring new ways to improve my skills in this area.
+                            When im not working on development projects, you can usually find me enjoying a good cup of coffee, exploring the great outdoors on a hike, or cycling through the city.
                             <br />
                             <br />
                             I have always been fascinated by the power of technology to solve problems and make the world a better place, which is why I decided to pursue a career in development.
-                            Whether I'm working on a new website or developing an innovative app, I find great satisfaction in using my skills to create something useful and impactful.
+                            Whether im working on a new website or developing an innovative app, I find great satisfaction in using my skills to create something useful and impactful.
                         </p>
                     </div>
                     <a target="_blank" href="https://drive.google.com/file/d/1Pk4My_kBeI8L-xl-0c_U5MKHH3ma0mb7/view?usp=share_link" className="button hover:cursor-pointer md:w-[50%] lg:w-[20%] mt-[2.5rem] md:mt-6 ml-10 md:ml-0 self-center text-center hover:scale-95 transition-all shadow-md bg-[#ffffff] pt-3 pb-3 pl-6 pr-6 md:pl-12 md:pr-12 rounded-[7px]" rel="noreferrer"><button ><span className="inline-block mr-2 pt-1"><IoNewspaperOutline /></span>Resume</button></a>
